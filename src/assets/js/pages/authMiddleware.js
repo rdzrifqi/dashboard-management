b26f9e0c1b1ec8138de19c674d25d3a59ab2ec94
rdzrifqi <rdzrifqi@gmail.com>
2026-04-29 10:08:26 +0700

    menghilangkan auto dark di sales activity

--- src/assets/js/pages/authMiddleware.js
+++ src/assets/js/pages/authMiddleware.js
@@ -1,17 +1,23 @@
 const token = localStorage.getItem("token");
+const name = localStorage.getItem("name");
+
 if (!token) {
     // not logged in, redirect to login page
-    window.location.href = "/login.html";
-}
-window.addEventListener('load', () => {
-    const path = window.location.pathname;
-    if (path==='/dashboard-sales-activity.html') {
-        Alpine.store('app').mode = 'dark';
-        Alpine.store('app').sidebar = true;
-        document.documentElement.classList.add('dark');
-    } else {
-        Alpine.store('app').mode = 'light';
-        Alpine.store('app').sidebar = false;
-        document.documentElement.classList.remove('dark');
+    if (name !== 'dessy') {
+        window.location.href = "/index.html";
+    }else{
+        window.location.href = "/sales_invoice_dim.html"
     }
-});
+}
+// window.addEventListener('load', () => {
+//     const path = window.location.pathname;
+//     if (path==='/dashboard-sales-activity.html') {
+//         Alpine.store('app').mode = 'dark';
+//         Alpine.store('app').sidebar = true;
+//         document.documentElement.classList.add('dark');
+//     } else {
+//         Alpine.store('app').mode = 'light';
+//         Alpine.store('app').sidebar = false;
+//         document.documentElement.classList.remove('dark');
+//     }
+// });
